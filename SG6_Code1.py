0ecee5deb6fab15c6f6333aad7927ffb4ff23b3d
JanAPandes <janalbert.pandes@smc.pshs.edu.ph>
2025-09-11 13:23:13 +0800

    SG6 Code #1
    This code shall ask the user to input three numbers that is accepted in the form of decimals. Based on input the code will add those numbers together, and if possible, will round off decimal digits by two digits. The built-in functions mainly consists of the input, round, and output functions.

---
+++ SG6_Code1.py
@@ -0,0 +1,10 @@
+# Libraries for Input/Output (I/O) Operations Code #1
+
+num1 = float(input("Input your first number:"))
+num2 = float(input("Input your second number:"))
+num3 = float(input("Input your third number:"))
+
+sum = float(num1 + num2 + num3)
+round(sum, 2)
+
+print("The total of the numbers is:", sum)
